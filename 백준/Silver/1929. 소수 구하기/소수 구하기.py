153d1736a416eb49f2348260cc562805b676a778
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-04-18 12:25:04 +0900

    [Silver III] Title: 소수 구하기, Time: 3668 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+import sys
+M, N = map(int, sys.stdin.readline().split())
+
+def check_prime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+for i in range(M, N + 1):
+    if check_prime(i):
+        print(i)
