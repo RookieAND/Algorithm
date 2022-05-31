0dd2f259453b1f3a5a2a6340153d97d8c7a6d8cd
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-05-31 14:51:25 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11524 ms, Memory: 215620 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+
+read = sys.stdin.readline
+N, M = map(int, read().split())
+graph = [list() for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, read().split())
+    graph[b].append(a)
+
+# BFS 탐색 알고리즘을 통해, 상위 노드와 연결된 하위 노드를 모두 찾음
+# 만약 노드를 탐색할 경우 방문 처리를 하고, 컴퓨터 수량에 1을 추가함.
+def bfs(start):
+    visited = [False] * (N + 1)
+    visited[start] = True
+    hacked, queue = 0, deque([start])
+    while queue:
+        v = queue.popleft()
+        for node in graph[v]:
+            if not visited[node]:
+                queue.append(node)
+                visited[node] = True
+                hacked += 1
+    return hacked
+
+
+# 모든 컴퓨터를 대상으로 최대 해킹 가능한 수량을 체크.
+result, is_hacked = [], 0
+for i in range(1, N + 1):
+    can_hacked = bfs(i)
+    if is_hacked < can_hacked:
+        is_hacked = can_hacked
+        result = [i]
+    elif is_hacked == can_hacked:
+        result.append(i)
+
+# 오름차순으로 나열하여 정답 출력
+print(*sorted(result))
