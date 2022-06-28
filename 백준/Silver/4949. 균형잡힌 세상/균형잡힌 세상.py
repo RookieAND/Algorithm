0d6a60ce58092e452249781e4f192d1f10624551
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-06-28 23:41:03 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 176 ms, Memory: 115828 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,3 +1,9 @@
+# 균형잡힌 문자열이 아닌 케이스
+
+# 1. 열린 괄호와 닫힌 괄호의 수량이 다른 경우. ((()), [(]
+# 2. 바깥 쪽의 괄호보다 안쪽의 괄호가 더 나중에 닫혔을 경우. ([ )]
+# ([ (([( [ ] ) ( ) (( ))] )) ]) => [), ], ), ), ], ),
+
 import sys
 
 bracket = {'(': ')', '[': ']'}
@@ -11,15 +17,13 @@ def check_sentence(stc):
             needed_bracket.append(bracket[char])
             continue
         if char == ')' or char == ']':
-            # 만약 더 이상 필요한 닫힌 괄호가 없다면, False 리턴.
-            if len(needed_bracket) == 0:
+            # 리스트가 비었거나 필요한 닫힌 괄호가 나오지 않았다면, False.
+            if not needed_bracket or needed_bracket[-1] != char:
                 return False
-            # 만약 필요한 닫힌 괄호가 나오지 않았다면, False 리턴
-            needed = needed_bracket.pop()
-            if needed != char:
-                return False
+            # 상단의 결격 사유가 없을 경우, 리스트에서 필요한 괄호를 제거.
+            needed_bracket.pop()
     # 남는 괄호 (닫히지 않은 괄호) 가 있다면, False 리턴
-    if len(needed_bracket) > 0:
+    if needed_bracket:
         return False
     return True
 
@@ -30,5 +34,4 @@ while True:
     # 종료 조건을 파악하기 위한 조건문
     if sentence == '.':
         break
-    result = check_sentence(sentence)
-    print('yes' if result else 'no')
+    print('yes' if check_sentence(sentence) else 'no')
