a835e88e8c93ca55ec6cab9f43535397b19b1f85
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-07-14 21:58:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 400 ms, Memory: 222212 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,5 +1,6 @@
 import sys
-count = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-arr.sort()
-print(arr[0], arr[len(arr) - 1])
+
+read = sys.stdin.readline
+N = int(read())
+number = list(map(int, read().split()))
+print(min(number), max(number))
