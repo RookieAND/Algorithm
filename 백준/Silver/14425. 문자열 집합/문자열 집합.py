3045bcec92ac284ee7f980911effa4fdb333e595
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-07-18 20:42:51 +0900

    [Silver III] Title: 문자열 집합, Time: 288 ms, Memory: 125648 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,18 @@
+import sys
+
+read = sys.stdin.readline
+N, M = map(int, read().split())
+
+S = [read().rstrip() for _ in range(N)]
+
+check = {}
+for _ in range(M):
+    sentence = read().rstrip()
+    if sentence not in check.keys():
+        check[sentence] = 1
+    else:
+        check[sentence] += 1
+
+print(sum([check[word] for word in S if word in check.keys()]))
+
+
