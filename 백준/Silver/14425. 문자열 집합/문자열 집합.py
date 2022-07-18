81108e9f9aea11e9708ff5a0ab84a8efec97bc1b
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-07-18 20:45:45 +0900

    [Silver III] Title: 문자열 집합, Time: 284 ms, Memory: 125660 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -8,11 +8,9 @@ S = [read().rstrip() for _ in range(N)]
 check = {}
 for _ in range(M):
     sentence = read().rstrip()
-    if sentence not in check.keys():
+    if sentence not in check:
         check[sentence] = 1
     else:
         check[sentence] += 1
 
-print(sum([check[word] for word in S if word in check.keys()]))
-
-
+print(sum([check[word] for word in S if word in check]))
