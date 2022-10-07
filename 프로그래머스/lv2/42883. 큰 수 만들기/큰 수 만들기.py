05d12afed468881a8919f8feadeb88a7b06b9694
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-10-07 10:46:33 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,8 @@
+def solution(number, k):
+    stack = []
+    for num in number:
+        while stack and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(num)
+    return "".join(stack[:len(stack) - k])
