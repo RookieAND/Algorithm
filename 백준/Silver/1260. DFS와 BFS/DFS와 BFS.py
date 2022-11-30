0e20f202a59a53dadc5b545dbe93c9ae25193dfc
Baik Gwangin <74497253+RookieAND@users.noreply.github.com>
2022-11-30 18:34:17 +0900

    [Silver II] Title: DFS와 BFS, Time: 116 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,38 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10 ** 6)
 
 read = sys.stdin.readline
 N, M, V = map(int, read().split())
-
-graph = [list() for _ in range(N + 1)]
+graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
-    f_node, s_node = map(int, read().split())
-    graph[f_node].append(s_node)
-    graph[s_node].append(f_node)
+    u, v = map(int, read().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-# 작은 노드부터 우선 탐색해야 하므로, 그래프를 1차적으로 정렬해야 함.
 graph = [sorted(graph[i]) for i in range(N + 1)]
 
-# DFS 탐색 함수, 재귀 함수를 사용하여 반복 실행된다.
-def dfs(start, visited):
-    visited[start] = True
-    print(start, end=" ")
-    for node in graph[start]:
+def dfs(x):
+    visited[x] = True
+    print(x, end=" ")
+    for node in graph[x]:
         if not visited[node]:
-            dfs(node, visited)
+            dfs(node)
 
-
-visited = [False] * (N + 1)
-dfs(V, visited)
-print("", end="\n")
-
-
-# BFS 탐색 함수, Queue를 사용한다는 것을 잊지 말자.
-def bfs(start, visited):
-    queue = deque([start])
-    visited[start] = True
+def bfs(x):
+    queue = deque([x])
+    visited[x] = True
     while queue:
-        v = queue.popleft()
-        print(v, end=" ")
-        for node in graph[v]:
+        current = queue.popleft()
+        print(current, end=" ")
+        for node in graph[current]:
             if not visited[node]:
                 queue.append(node)
                 visited[node] = True
 
-
 visited = [False] * (N + 1)
-bfs(V, visited)
-
+dfs(V)
+print("")
+visited = [False] * (N + 1)
+bfs(V)
