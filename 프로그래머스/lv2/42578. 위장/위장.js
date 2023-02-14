a2e80ed949811cfdf147e03d904d59066511908b
Gwangin Baik <gwangin1999@naver.com>
2023-02-14 20:10:37 +0900

    [level 2] Title: 위장, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,7 +1,5 @@
 function solution(clothes) {
     const closet = new Map();
     clothes.forEach(([, key]) => closet.set(key, !closet.get(key) ? 1 : closet.get(key) + 1));
-    const clothAmount = Array.from(closet.values());
-    const answer = clothAmount.reduce((acc, cur) => acc * (cur + 1), 1) - 1;
-    return answer;
+    return Array.from(closet.values()).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
 }
