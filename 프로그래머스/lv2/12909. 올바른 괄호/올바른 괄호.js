e8ab09dd309204699cdabc77e81a87b2b3e31174
Gwangin Baik <gwangin1999@naver.com>
2023-02-16 19:58:29 +0900

    [level 2] Title: 올바른 괄호, Time: 5.68 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,19 +1,12 @@
-// '(' 열린 괄호를 만나면 stack 에 이를 추가하고, 닫힌 괄호를 만나면 stack에서 제거한다.
 function solution(s){
     let openBracket = 0
 
     for (const char of s) {
-        // 열린 괄호를 만났다면 수량을 1개 추가하고 다음 반복을 진행.
-        if (char == '(') {
-            openBracket += 1;
-            continue
-        }
-        // 만약 열린 괄호가 없는데 닫힌 괄호가 들어왔다면 false.
-        if (!openBracket) {
-            return false
-        }
-        openBracket -= 1;
+        // 열린 괄호인지, 닫힌 괄호인지에 따라 열린 괄호의 수량을 더하거나 빼줌.
+        openBracket += char == '(' ? 1 : -1
+        // 만약 열린 괄호의 수량이 0 혹은 그 이하로 떨어진다면 false
+        if (openBracket < 0) return false
     }
-    // 괄호가 완전히 열리고 닫혀 stack 내부가 비었다면 true, 아니라면 false
+    // 괄호가 완전히 열리고 닫혀 수량이 0개라면 true, 아니라면 false
     return openBracket === 0;
 }
