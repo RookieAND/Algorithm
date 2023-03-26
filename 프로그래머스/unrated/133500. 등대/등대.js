e70ac17ec0d9765a0eac6c0a938e1c5c4ec61acb
Gwangin Baik <gwangin1999@naver.com>
2023-03-26 23:51:59 +0900

    feat: 등대 문제 업그레이드 풀이

--- 프로그래머스/unrated/133500. 등대/등대.js
+++ 프로그래머스/unrated/133500. 등대/등대.js
@@ -1,38 +1,29 @@
 function solution(n, lighthouse) {
-    const memo = new Array(n + 1).fill(false);
+    const visited = new Array(n + 1).fill(false);
     let result = 0;
-    
-    while(lighthouse.length) {      
-      const map = new Array(n + 1).fill().map(_ => []);
-        
-      for (const el of lighthouse) {
-        const [a, b] = el;
-            
-        map[a].push(b);
-        map[b].push(a);
-      }
-        
-      // 탐욕법
-      map.filter(el => el.length === 1).forEach((el) => {
-          const [target] = el;
-          if (!memo[target]) {
-              memo[target] = true;
-              if (map[target].length !== 1) {
-                  result += 1;
-              } else {
-              	  // 양쪽 모두 있는 경우이므로 더하기가 두번일어난다.
-                  result += 0.5;
-              } 
-          }
-      });
-        
-      // 간선이 1개인 섬 모두 제거
-      lighthouse = lighthouse.filter((el) => {
-        const [a, b] = el;
-            
-        return !memo[a] && !memo[b];
-      });
+
+    while (lighthouse.length) {
+        const matrix = Array.from({ length: n + 1 }, () => []);
+
+        for (const [a, b] of lighthouse) {
+            matrix[a].push(b);
+            matrix[b].push(a);
+        }
+
+        // 각 말단 노드를 순회하여, 해당 노드가 아직 미방문된 상태라면 방문 처리를 진행한다.
+        // 만약 해당 말단 노드와 연결된 등대의 간선이 1개 이상이라면 계산이 두 차례 발생하므로 0.5를 더해야 한다.
+        matrix
+            .filter((el) => el.length === 1)
+            .forEach(([target]) => {
+                if (!visited[target]) {
+                    visited[target] = true;
+                    result += matrix[target].length !== 1 ? 1 : 0.5;
+                }
+            });
+
+        // 간선이 1개인 등대를 모두 제거하고, 반복을 재개한다.
+        lighthouse = lighthouse.filter(([a, b]) => !visited[a] && !visited[b]);
     }
-  
+
     return result;
-  }
+}
