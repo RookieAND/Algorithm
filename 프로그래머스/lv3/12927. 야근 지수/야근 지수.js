c70284f0d07828beacddca75daf0a81edf9c834f
Gwangin Baik <gwangin1999@naver.com>
2023-05-14 23:43:00 +0900

    chore: 풀이 주석 추가

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
@@ -1,20 +1,45 @@
+// 어떤 일을 줄였을 때, 가장 효율적으로 피로도가 줄어들 수 있는가.
+// 결국 각각의 일을 "최대한" 균등하게 나누는 작업이 필요하다. (제곱이기 때문)
+
 function solution(n, works) {
-  if(works.reduce((a,b) => a+b) <= n) return 0;
-  
-  let sorted = works.sort((a,b) => a-b);
-  const len = works.length;
-  
-  while(n) {
-    const max = sorted[len-1];
-    
-    for(let i = len-1; i >= 0; i--) {
-      if(sorted[i] >= max) {
-        sorted[i]--;
+  // 예외 : 일을 다 끝마칠 수 있는지부터 체크한 후에 return 시키자.
+  let wholeWorks = works.reduce((acc, cur) => acc + cur, 0);
+  if (wholeWorks <= n) return 0;
+
+  const result = works.sort((a, b) => a - b);
+  const workAmount = works.length;
+
+  while (n > 0) {
+    const biggestWork = result.at(-1);
+    for (let i = workAmount - 1; i >= 0; i--) {
+      if (result[i] >= biggestWork) {
+        result[i]--;
         n--;
       }
-      if(!n) break;
+      if (!n) break;
     }
   }
-  
-  return sorted.reduce((a,b) => a + Math.pow(b, 2), 0);
+
+  return result.reduce((acc, cur) => acc + cur ** 2, 0);
 }
+
+console.log(solution(2, [3, 5, 6, 8]));
+
+// // 오답 : 효율성 테스트 실패
+// function solution(n, works) {
+//   // 예외 : 일을 다 끝마칠 수 있는지부터 체크한 후에 return 시키자.
+//   works.sort();
+//   let wholeWorks = works.reduce((acc, cur) => acc + cur, 0);
+//   if (wholeWorks <= n) return 0;
+
+//   // 그게 아니라면 가장 일이 "많은" 작업을 쳐내자.
+//   while (wholeWorks > 0 && n > 0) {
+//     const biggestWork = Math.max(...works);
+//     const biggestWorkIndex = works.findIndex((value) => value === biggestWork);
+//     works[biggestWorkIndex] -= 1;
+//     wholeWorks -= 1;
+//     n -= 1;
+//   }
+
+//   return wholeWorks ? works.reduce((acc, cur) => acc + cur ** 2, 0) : 0;
+// }
